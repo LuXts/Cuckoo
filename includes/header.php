fe4439efb8cd71b1749f6cad43178e0f58417690
LuXts <1776137198@qq.com>
2020-02-29 03:52:50 +0800

    test

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
  * 
  * @author Bhao
  * @link https://dwd.moe/
- * @version 0.0.7(Beta)
+ * @version 1.0.0
  */
 ?>
 <html>
@@ -21,14 +21,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
   <title><?php $this->archiveTitle(array('category' => _t('分类 %s 下的文章'), 'search' => _t('包含关键字 %s 的文章'), 'tag' => _t('标签 %s 下的文章'), 'author' => _t('%s 发布的文章')), '', ' - ');$this->options->title(); ?></title>
   <link rel="shortcut icon" href="<?php favicon() ?>" /> 
-  <link rel="stylesheet" href="<?php staticFiles('assets/css/mdui.min.css') ?>">
+  <link rel="stylesheet" href="<?php staticFiles('assets/css/mdui.css') ?>">
   <link rel="stylesheet" href="<?php staticFiles('assets/css/iconfont.css') ?>">
   <link rel="stylesheet" href="<?php staticFiles('assets/css/tocbot.css') ?>">
   <link rel="stylesheet" href="<?php staticFiles('assets/css/cuckoo.css'); ?>">
   <link rel="stylesheet" href="<?php staticFiles('assets/css/nprogress.css'); ?>">
-  <link rel="stylesheet" href="<?php staticFiles('assets/css/atom-one-dark.min.css'); ?>">
+  <link rel="stylesheet" href="<?php staticFiles('assets/css/atom-one-dark.css'); ?>">
+  <link rel="stylesheet" href="<?php staticFiles('assets/css/share.css'); ?>">
   <script src="<?php staticFiles('assets/js/nprogress.js') ?>"></script>
-  <script src="<?php staticFiles('assets/js/tocbot.min.js') ?>"></script>
+  <script src="<?php staticFiles('assets/js/tocbot.js') ?>"></script>
+  <script src="<?php staticFiles('assets/js/social-share.js') ?>"></script>
   <?php $this->header(); bgUrl(); ?>
 </head>
 
